81cafe8cbc4c24eeb16fdf020a173754fce194d7
waradon555 <waradon555@gmail.com>
2021-10-15 20:00:50 +0700

    added placeholder for player character

--- gametest.py
+++ gametest.py
@@ -4,14 +4,15 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((450, 600))
-icon = pygame.image.load('meteor/asset/icon.png')
+icon = pygame.image.load('asset/icon.png') # 100*100 or smaller
 pygame.display.set_caption('Meteor Panic!')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-ground_surface = pygame.image.load('meteor/asset/ground.png')
-sky_surface = pygame.image.load('meteor/asset/sky.png')
-exo_surface = pygame.image.load('meteor/asset/fallen_sky.png')
+ground_surface = pygame.image.load('asset/ground.png') # 450*100
+sky_surface = pygame.image.load('asset/sky.png') # 450*600 
+exo_surface = pygame.image.load('asset/fallen_sky.png') # 450*125
+character = pygame.image.load('asset/character.png') # 65*100
 
 while True:
     for event in pygame.event.get():
@@ -22,6 +23,7 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(exo_surface, (0,0))
     screen.blit(ground_surface, (0, 500))
+    screen.blit(character, (190, 400))
     # update thing
     pygame.display.update()
     clock.tick(60)
