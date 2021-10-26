a2ed0550378bdf558d244ee03ae47946e08e529c
waradon555 <waradon555@gmail.com>
2021-10-26 20:02:03 +0700

    simple motion test

--- gametest.py
+++ gametest.py
@@ -3,16 +3,17 @@ from sys import exit
 
 
 pygame.init()
-screen = pygame.display.set_mode((450, 600))
+screen = pygame.display.set_mode((450, 600)) #450*600 
 icon = pygame.image.load('asset/icon.png') # 100*100 or smaller
 pygame.display.set_caption('Meteor Panic!')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-ground_surface = pygame.image.load('asset/ground.png') # 450*100
-sky_surface = pygame.image.load('asset/sky.png') # 450*600 
-exo_surface = pygame.image.load('asset/fallen_sky.png') # 450*125
-character = pygame.image.load('asset/character.png') # 65*100
+ground_surface = pygame.image.load('asset/ground.png').convert() # 450*100
+sky_surface = pygame.image.load('asset/sky.png').convert() # 450*600 
+exo_surface = pygame.image.load('asset/fallen_sky.png').convert() # 450*125
+character = pygame.image.load('asset/character.png').convert_alpha() # 65*100
+char_pos_x = 190
 
 while True:
     for event in pygame.event.get():
@@ -23,7 +24,10 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(exo_surface, (0,0))
     screen.blit(ground_surface, (0, 500))
-    screen.blit(character, (190, 400))
+    screen.blit(character, (char_pos_x, 400))
+    char_pos_x += 4
+    if char_pos_x > 600:
+        char_pos_x = - 50
     # update thing
     pygame.display.update()
     clock.tick(60)
