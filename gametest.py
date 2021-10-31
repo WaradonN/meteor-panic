6d9cf0cf89a85d6bab196fbc483758923d73f7de
waradon555 <waradon555@gmail.com>
2021-10-31 13:24:24 +0700

    added font

--- gametest.py
+++ gametest.py
@@ -8,7 +8,9 @@ icon = pygame.image.load('asset/icon.png') # 100*100 or smaller
 pygame.display.set_caption('Meteor Panic!')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
+gamefont = pygame.font.Font('asset/PressStart2P.ttf', 20)
 
+### stuff to draw
 ground_surface = pygame.image.load('asset/ground.png').convert() # 450*100
 sky_surface = pygame.image.load('asset/sky.png').convert() # 450*600 
 exo_surface = pygame.image.load('asset/fallen_sky.png').convert() # 450*125
@@ -25,9 +27,9 @@ while True:
     screen.blit(exo_surface, (0,0))
     screen.blit(ground_surface, (0, 500))
     screen.blit(character, (char_pos_x, 400))
-    char_pos_x += 4
-    if char_pos_x > 600:
-        char_pos_x = - 50
+    char_pos_x += 3
+    if char_pos_x > 500:
+        char_pos_x = - 65
     # update thing
     pygame.display.update()
     clock.tick(60)
