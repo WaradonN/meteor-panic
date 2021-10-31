d2a68f87ff045325a88fb8be33eb6516b569361c
waradon555 <waradon555@gmail.com>
2021-10-31 14:22:26 +0700

    simple movement

--- meteor.py
+++ meteor.py
@@ -1,7 +1,9 @@
 import pygame
 from sys import exit
 
-### meta and inner works
+from pygame.constants import KEYDOWN, K_d
+
+# meta and inner works
 pygame.init()
 screen = pygame.display.set_mode((450, 600)) #450*600 
 icon = pygame.image.load('asset/icon.png') # 100*100 or smaller
@@ -10,27 +12,43 @@ pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 gamefont = pygame.font.Font('asset/PressStart2P.ttf', 20)
 
-### stuff to draw
+# stuff to draw
 ground_surface = pygame.image.load('asset/ground.png').convert() # 450*100
 sky_surface = pygame.image.load('asset/sky.png').convert() # 450*600 
 exo_surface = pygame.image.load('asset/fallen_sky.png').convert() # 450*125
 character = pygame.image.load('asset/character.png').convert_alpha() # 65*100
 char_pos_x = 190
 
-### input and updates
+# input and updates
 while True:
-    ### input
+    ## input
+    key = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw stuff
+        if key[pygame.K_d]: # Go Right
+            if char_pos_x >= 385:
+                char_pos_x += 0
+            elif char_pos_x < 385:
+                char_pos_x += 6
+        elif key[pygame.K_a]: # Go Left
+            if char_pos_x <= 0:
+                char_pos_x -= 0
+            if char_pos_x > 0:
+                char_pos_x -= 6
+    ## draw stuff
+    ### enviroment
     screen.blit(sky_surface, (0,0))
     screen.blit(exo_surface, (0,0))
     screen.blit(ground_surface, (0, 500))
+    ### stuff that change
     screen.blit(character, (char_pos_x, 400))
-    if char_pos_x > 450:
-        char_pos_x = - 50
-    # update thing
+    ### game state (wrongly used technical word, wow!)
+    # if char_pos_x <= 0:
+    #     char_pos_x = 0
+    # if char_pos_x >= 385:
+    #     char_pos_x = 385
+    ## update thing
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(90)
