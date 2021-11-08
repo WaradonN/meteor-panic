b92d7d5894487e5c2f6227c287ec0f51896fa3e3
waradon555 <waradon555@gmail.com>
2021-11-08 15:01:40 +0700

    time format

--- meteor.py
+++ meteor.py
@@ -3,10 +3,18 @@ import random
 from sys import exit
 
 ###### functions and good stuffs ######
+def mili2format(time): #00:00:000
+    mili = time
+    sec = mili // 1000
+    mili_left = mili % 1000
+    minu = sec // 60
+    sec_left = sec % 60
+    return "%02d:%02d:%03d" %(minu,sec_left,mili_left)
+
 def display_score():
     """displaying times simple"""
     survive_time = pygame.time.get_ticks() - start_time
-    time_surf = gamefont.render(str(survive_time // 1000), False, 'black')
+    time_surf = gamefont.render(mili2format(survive_time), False, 'black')
     time_rect = time_surf.get_rect(topright = (425,60))
     time_rect_rtt = pygame.transform.rotozoom(time_surf,10,1)
     screen.blit(time_rect_rtt, time_rect)
@@ -25,7 +33,7 @@ gamefont = pygame.font.Font('asset/PressStart2P.ttf', 13)
 main_menu = True
 alive = False
 start_time = 0
-final_time = 0
+final_time = mili2format(0)
 
 # stuff to draw
 main_menu_surf = pygame.image.load('asset/main_menu.png').convert()
@@ -41,7 +49,10 @@ gameover = pygame.image.load('asset/gameover.png').convert_alpha()
 character_rect = character.get_rect(topleft = (200, 400))
 meteor_rect = meteor.get_rect(center = (400, 475))
 
-# input and updates
+# game state
+meteor_time = pygame.USEREVENT + 1
+pygame.time.set_timer(meteor_time, random.randint(900, 1100))
+
 while True:
     key = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -57,7 +68,7 @@ while True:
             alive = True
     ###############################
 
-    ## input
+    ########## Input ##########
     if key[pygame.K_d] and alive: # Go Right
         if character_rect.x >= 385:
             character_rect.x += 0
@@ -72,19 +83,20 @@ while True:
         character_rect.x = 200
         alive = True
         start_time = pygame.time.get_ticks()
-    if key[pygame.K_ESCAPE] and alive == False:
-        main_menu = True
+    if key[pygame.K_ESCAPE]:
+            main_menu = True
+    ###########################
     
     ########## The Game Itself ##########
     if alive and main_menu == False:
         ### enviroment
-        final_time = display_score()
+        final_time = mili2format(display_score())
         screen.blit(sky_surface, (0,0))
         screen.blit(exo_surface, (0,0))
         screen.blit(ground_surface, (0, 500))
-        screen.blit(gamefont.render('minus zeta build ;(', False, 'black'), (10, 10))
+        screen.blit(gamefont.render('Press \'esc\' to quit', False, 'black'), (10, 10))
         display_score()
-        ### stuff that change
+        ### stuff that changeddd
         screen.blit(meteor, meteor_rect)
         if alive:
             screen.blit(character, character_rect)
@@ -94,7 +106,7 @@ while True:
         if character_rect.colliderect(meteor_rect):
             alive = False
     elif alive == False and main_menu == False:
-        final = gamefont.render('Your time : %d' %final_time, False, 'white')
+        final = gamefont.render('Your time : %s' %final_time, False, 'white')
         final_rect = final.get_rect(center = (220, 300))
         screen.blit(character_expired, character_rect)
         screen.blit(gameover, (0,0))
